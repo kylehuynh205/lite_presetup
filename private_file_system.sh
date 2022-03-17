213ffd8054e9b4b4aa92968a3b4d3b3cea6805bd
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-03-17 14:50:55 -0400

    Update private_file_system.sh

--- private_file_system.sh
+++ private_file_system.sh
@@ -5,9 +5,10 @@ public_files_path=/var/www/html/drupal/web/sites/default/files
 private_files_path=/var/www/html/drupal/web/sites/default/private_files
 
 mkdir "${private_files_path}"
+chown -Rf www-data:www-data "${private_files_path}"
 
 # update settings.php with path of private file system
-sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" settings.php
+sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" /var/www/html/drupal/web/sites/default/settings.php
 
 # configure access control fields
 drush -y config-import --partial --source=$PWD/config/private_file_system
