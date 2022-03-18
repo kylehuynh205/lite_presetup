c396531ef1a2083d22d9f38261497a108e3aab63
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-03-18 11:43:49 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,6 @@ site_path=$PWD/..
 
 
 #Enable modules
-drush -y pm:enable responsive_image syslog devel admin_toolbar pdf matomo restui controlled_access_terms_defaults jsonld field_group field_permissions features file_entity view_mode_switch islandora_defaults islandora_marc_countries chart_suite openseadragon chart_suite ableplayer islandora_iiif islandora_display advanced_search media_thumbnails media_thumbnails_pdf media_thumbnails_video csv_importer advancedqueue_runner triplestore_indexer fits
+drush -y pm:enable responsive_image syslog devel admin_toolbar pdf matomo restui controlled_access_terms_defaults jsonld field_group field_permissions features file_entity view_mode_switch islandora_defaults islandora_marc_countries chart_suite openseadragon chart_suite ableplayer islandora_iiif islandora_display advanced_search media_thumbnails media_thumbnails_pdf media_thumbnails_video media_library_edit csv_importer advancedqueue_runner triplestore_indexer fits
 
 
