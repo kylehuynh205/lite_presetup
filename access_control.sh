7dc24a14554925aef0b8a611e8184aef4e77a22b
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-03-18 11:11:20 -0400

    Update access_control.sh

--- access_control.sh
+++ access_control.sh
@@ -10,7 +10,7 @@ private_files_path="${site_path}"/web/sites/default/private_files
 drush -y pm:enable group groupmedia group_permissions gnode islandora_group_defaults islandora_group group_solr
 
 mkdir "${private_files_path}"
-chown -Rf www-data:www-data "${private_files_path}"
+#chown -Rf www-data:www-data "${private_files_path}"
 
 # update settings.php with path of private file system
 chmod 777 "${site_path}"/web/sites/default/settings.php && sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" "${site_path}"/web/sites/default/settings.php && chmod 444 "${site_path}"/web/sites/default/settings.php
