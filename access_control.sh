d4c6c7396c871f04e1cc02a9a4855bb9f7ee90de
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-03-18 14:12:08 -0400

    Update access_control.sh

--- access_control.sh
+++ access_control.sh
@@ -16,7 +16,9 @@ mkdir "${private_files_path}"
 #chown -Rf www-data:www-data "${private_files_path}"
 
 # update settings.php with path of private file system
-chmod 777 "${site_path}"/web/sites/default/settings.php && sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" "${site_path}"/web/sites/default/settings.php && chmod 444 "${site_path}"/web/sites/default/settings.php
+chmod 777 "${site_path}"/web/sites/default/settings.php 
+
+cd "${site_path}"/web/sites/default && sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" settings.php && chmod 444 "${site_path}"/web/sites/default/settings.php && cd "${inital_path}"
 
 # configure access control fields
 drush -y config-import --partial --source=$PWD/config/private_file_system
