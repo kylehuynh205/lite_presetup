a8e9d22a93a25e06288f20561032df4de48c9e50
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-03-18 10:46:03 -0400

    Update access_control.sh

--- access_control.sh
+++ access_control.sh
@@ -1,5 +1,5 @@
 #current site
-site_path=$PWD/../
+site_path=$PWD/..
 
 public_files_path="${site_path}"/web/sites/default/files
 private_files_path="${site_path}"/web/sites/default/private_files
@@ -11,7 +11,7 @@ mkdir "${private_files_path}"
 chown -Rf www-data:www-data "${private_files_path}"
 
 # update settings.php with path of private file system
-cd "${site_path}"/web/sites/default && chmod 777 settings.php && sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" "${site_path}"/web/sites/default/settings.php && chmod 444 settings.php
+chmod 777 "${site_path}"/web/sites/default/settings.php && sed -i "/file_private_path/c\$settings['file_private_path'] = 'sites/default/private_files';" "${site_path}"/web/sites/default/settings.php && chmod 444 "${site_path}"/web/sites/default/settings.php
 
 # configure access control fields
 drush -y config-import --partial --source=$PWD/config/private_file_system
@@ -19,8 +19,7 @@ drush -y config-import --partial --source=$PWD/config/private_file_system
 
 # Apply patch for file_entity
 wget https://raw.githubusercontent.com/digitalutsc/override_permission_file_entity/main/override_file_access.patch -P "${site_path}"/web/modules/contrib/file_entity
-#cd "${site_path}"/web/modules/contrib/file_entity && patch -p1 < override_file_access.patch
-patch -u "${site_path}"web/modules/contrib/file_entity/file_entity.module -i "${site_path}"web/modules/contrib/file_entity/override_file_access.patch
+patch -u "${site_path}"/web/modules/contrib/file_entity/file_entity.module -i "${site_path}"/web/modules/contrib/file_entity/override_file_access.patch
 
 # import access control fields
 drush -y config-import --partial --source=$PWD/config/access_control
