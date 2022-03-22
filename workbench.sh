509b3f3cf3830544d22fcc3e55a2eade76701b9f
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-03-22 11:36:55 -0400

    Create workbench.sh

---
+++ workbench.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#inital_path
+inital_path=$PWD
+
+#current site
+site_path="${inital_path}"/..
+
+wget https://raw.githubusercontent.com/digitalutsc/isle-dc/islandora_lite/scripts/workbench_integration.patch -P ${site_path}/web/modules/contrib/islandora_workbench_integration
+	(cd ${site_path}/web/modules/contrib/islandora_workbench_integration && patch -p1 < workbench_integration.patch)
+drush -y pm:enable islandora_workbench_integration
