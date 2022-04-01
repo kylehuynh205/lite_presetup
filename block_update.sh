7136d1823509cf2a081cf6f264f7ec21cffb6319
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-04-01 14:46:42 -0400

    Create block_update.sh

---
+++ block_update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DRUPAL_THEME = olivero
+drush -y config-import --partial --source /var/www/drupal/islandora_lite_installation/configs/blocks_$(DRUPAL_THEME)
+
+DOMAIN=$1
+DRUPAL_DEFAULT_ACCOUNT_PASSWORD=$2
+URL="https://${DOMAIN}/term_from_uri?_format=json&uri=https%3A%2F%2Fschema.org%2FBook"
+BOOK_TERM_ID=$(curl -u admin:"$DRUPAL_DEFAULT_ACCOUNT_PASSWORD" -X GET "$URL" | jq .[].tid[].value)
+echo "book term id: $BOOK_TERM_ID"
+MANIFEST_URL="https://${DOMAIN}/node/[node:nid]/book-manifest"
+
+# Set book-manifest url
+drush -y --input-format=yaml config:set block.block.miradorblock visibility.term "
+id: term
+tid:
+  -
+    target_id: '$BOOK_TERM_ID'
+negate: false
+context_mapping: {  }"
+
+# Set book-manifest url
+drush -y config:set block.block.miradorblock settings.iiif_manifest_url $MANIFEST_URL
