53bc9032837fdbc02349fbab2045f3025aafacb7
Kyle Huynh <7862086+kylehuynh205@users.noreply.github.com>
2022-04-05 19:17:25 -0400

    Update advanced_search.sh

--- scripts/advanced_search.sh
+++ scripts/advanced_search.sh
@@ -8,3 +8,6 @@ site_path="${inital_path}"/../..
 
 # import advanced search configs
 drush -y config-import --partial --source=$"${inital_path}"/../configs/advanced_search
+
+# enable Lunce search
+drush -y config-set --input-format=yaml advanced_search.settings lucene_on_off 1
